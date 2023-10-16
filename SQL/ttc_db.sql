6c9049b2464835e56fcb8fbcc99043066290e2d6
MAamer28 <130619866+MAamer28@users.noreply.github.com>
2023-10-15 22:14:22 -0400

    Oct15_v1
    Added SQL database construction and reading SQL DB into Jupyter Notebook via sqlalchemy as a Pandas dataframe.

---
+++ SQL/ttc_db.sql
@@ -0,0 +1,17 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE bus_delay_2022 (
+    Date date,
+    Route varchar(200),
+    Time time,
+    Day varchar(200),
+    Location varchar(200),
+    Incident varchar(200),
+    Min_Delay int,
+    Min_Gap int,
+    Direction varchar(200),
+    Vehicle int   
+);
+
